34b61e768ddf4adaaf51a38f8d23844b274e174f
JasmineAufiero <jasmineaufiero@gmail.com>
2022-06-07 16:01:13 +0200

    Initial Commit

---
+++ Med_Waste/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Med_Waste
+//
+//  Created by Jasmine Aufiero on 07/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Med_Waste/Med_WasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Med_WasteApp.swift
+//  Med_Waste
+//
+//  Created by Jasmine Aufiero on 07/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Med_WasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
