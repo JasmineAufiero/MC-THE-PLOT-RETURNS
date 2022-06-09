cc4b70a947b64b5eb8362ec321516b00ee7b743b
JasmineAufiero <jasmineaufiero@gmail.com>
2022-06-09 13:54:24 +0200

    new item working again!

--- Med_Waste/Views/Sheets/NewItemView.swift
+++ Med_Waste/Views/Sheets/NewItemView.swift
@@ -21,172 +21,165 @@ struct NewItemView: View {
     @State var categoria = ""
     @State var expirationDate :[Date] = [Date.now] // an array of expiration date for different boxes
     
+//        .focused($amountIsFocused)
+    
     @Binding var showData: Bool
-    
-    
     var medicineViewModel :MedicineViewModel
     var boxViewModel :BoxViewModel
     
-    
     var body: some View {
-        
-        NavigationView {
             
-            VStack(alignment: .leading, spacing: 0) {
+            NavigationView {
                 
-                Text("Conferma Informazioni")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .multilineTextAlignment(.leading)
-                    .padding(.horizontal,20)
-                
-                Form {
+                VStack(alignment: .leading, spacing: 0) {
                     
-                    Section {
-                        HStack {
-                            Text("Nome")
-                                .fontWeight(.semibold)
-                            Spacer()
-                            TextField("Nome", text: $nome)
-                                .multilineTextAlignment(.trailing)
-                                .focused($amountIsFocused)
-                            
-                        }
-                        HStack {
-                            Text("Dosaggio")
-                                .fontWeight(.semibold)
-                            Spacer()
-                            TextField("Dosaggio", text: $dosaggio)
-                                .multilineTextAlignment(.trailing)
-                                .focused($amountIsFocused)
-                        }
-                        HStack {
-                            Text("Tipogia")
-                                .fontWeight(.semibold)
-                            Spacer()
-                            TextField("Tipologia", text: $tipologia)
-                                .multilineTextAlignment(.trailing)
-                                .focused($amountIsFocused)
-                            //
-                        }
-                        HStack {
-                            Text("Prezzo")
-                                .fontWeight(.semibold)
-                            Spacer()
-                            TextField("Prezzo", text: $prezzo)
-                                .multilineTextAlignment(.trailing)
-                                .keyboardType(.decimalPad)
-                                .focused($amountIsFocused)
-                        }
-                        HStack {
-                            Text("Unità")
-                                .fontWeight(.semibold)
-                            Spacer()
-                            TextField("Unità", text: $unità)
-                                .multilineTextAlignment(.trailing)
-                                .keyboardType(.decimalPad)
-                                .focused($amountIsFocused)
-                            
-                        }
-                        
-                    } // :Information section
-                    .padding()
-                    .listRowBackground(CustomColor.blueform)
-                    
-                    // adding the number of boxes
-                    
-                    Group {
-                        Section {
-                            HStack {
-                                Text("Numero di scatole:  \(numerobox)")
-                                    .fontWeight(.semibold)
-                                
-                                StepperView(expirationDates: $expirationDate, numerobox: $numerobox)
-                                
-//                                Stepper("", onIncrement: {
-//                                    if numerobox < 50 { numerobox += 1
-//                                    }
-//                                    expirationDate.insert(Date.now, at: numerobox-1)
-//
-//                                }, onDecrement: {
-//                                    if numerobox > 1 { numerobox -= 1
-//                                        expirationDate.remove(at: numerobox-1) }
-//
-//                                })
-                                        
-//                                Stepper("", value: $numberobox, in: 1...50)
-//                                    .multilineTextAlignment(.trailing)
-                            }
-                            
-                        }.listRowBackground(CustomColor.blueform).padding()
-                        // : section for stepper
-                        
-                    }
-                    
-                    
-                    Text("Date di Scadenza")
+                    Text("Conferma Informazioni")
                         .font(.title2)
                         .fontWeight(.semibold)
                         .multilineTextAlignment(.leading)
+                        .padding(.horizontal,20)
                     
-                    
-                    CheckBoxView(isTheSameDate: $isTheSameDate)
-                        .onTapGesture {
-                            isTheSameDate.toggle()
-                        }
-                    
-                    
-                    // modified: check if the used ask to add more than one box with the same expiration date
-                    Section {
-                        ForEach(1...numerobox, id: \.self) { index in
-                            
+                    Form {
+                        
+                        Section {
                             HStack {
-                                DatePicker("Box  \(index)", selection:
-                                              
-                                            // if the user wants all the boxes have the same expiration date,it automatically update the same date, else each box has a different expiration date
-                                           isTheSameDate ? $expirationDate[0] : $expirationDate[index-1]
-                                            
-                                            
-                                            , in: Date()..., displayedComponents: .date)
-                                
+                                Text("Nome")
+                                    .fontWeight(.semibold)
                                 Spacer()
+                                TextField("Nome", text: $nome)
+                                    .multilineTextAlignment(.trailing)
                                 
-                                Button {} label: {
-                                    Image(systemName: "viewfinder") // add the possibility to scan
-                                }
+                            }
+                            HStack {
+                                Text("Dosaggio")
+                                    .fontWeight(.semibold)
+                                Spacer()
+                                TextField("Dosaggio", text: $dosaggio)
+                                    .multilineTextAlignment(.trailing)
+                            }
+                            HStack {
+                                Text("Tipogia")
+                                    .fontWeight(.semibold)
+                                Spacer()
+                                TextField("Tipologia", text: $tipologia)
+                                    .multilineTextAlignment(.trailing)
+                                //
+                            }
+                            HStack {
+                                Text("Prezzo")
+                                    .fontWeight(.semibold)
+                                Spacer()
+                                TextField("Prezzo", text: $prezzo)
+                                    .multilineTextAlignment(.trailing)
+                            }
+                            HStack {
+                                Text("Unità")
+                                    .fontWeight(.semibold)
+                                Spacer()
+                                TextField("Unità", text: $unità)
+                                    .multilineTextAlignment(.trailing)
                             }
                             
-                        }
-                    }
-                    .listRowBackground(CustomColor.redform).padding()
-                    // : section for stepper
-                    
-                    
-//                    Text("Categoria")
-//                        .font(.title2)
-//                        .fontWeight(.semibold)
-//                        .multilineTextAlignment(.leading)
-//
-//
-//                    VStack(spacing: 20) {
-//                        Picker(selection: $categoria, label: Text("Categoria Medicinale")) {
-//                            Text("Antistaminici").tag(1)
-//                            Text("Antivirali").tag(2)
-//                            Text("Anti-Infiammatori").tag(3)
-//                            Text("Antibiotici").tag(4)
-//                            Text("Antimicotici").tag(5)
-//                            Text("Analgesici").tag(6)
-//                        }
-//                    }
-                    
-//                    Spacer()
-                    
-                    
-                    // "Done Button" it allows to add an element inside the cabinet view
-               
+                        } // :Information section
+                        .padding()
+                        .listRowBackground(Color.init(red: 202/255, green: 230/255, blue: 247/255))
                         
-                        Button(action: {
-                            medicineViewModel.addNewMedicine(name: nome, dosage: dosaggio, type: tipologia, price: prezzo, units: Int(unità) ?? 0, category: "antistaminico",isPinned: false)
+                        // adding the number of boxes
+                        
+                        Group {
+                            Section {
+                                HStack {
+                                    Text("Numero di scatole:  \(numerobox)")
+                                        .fontWeight(.semibold)
+                                    
+                                    StepperView(expirationDates: $expirationDate, numerobox: $numerobox)
+                                    
+    //                                Stepper("", onIncrement: {
+    //                                    if numerobox < 50 { numerobox += 1
+    //                                    }
+    //                                    expirationDate.insert(Date.now, at: numerobox-1)
+    //
+    //                                }, onDecrement: {
+    //                                    if numerobox > 1 { numerobox -= 1
+    //                                        expirationDate.remove(at: numerobox-1) }
+    //
+    //                                })
+                                            
+    //                                Stepper("", value: $numberobox, in: 1...50)
+    //                                    .multilineTextAlignment(.trailing)
+                                }
+                                
+                            }.listRowBackground(Color.init(red: 202/255, green: 230/255, blue: 247/255)).padding()
+                            // : section for stepper
+                            
+                        }
+                        
+                        
+                        Text("Date di Scadenza")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.leading)
+                        
+                        
+                        CheckBoxView(isTheSameDate: $isTheSameDate)
+                            .onTapGesture {
+                                isTheSameDate.toggle()
+                            }
+                        
+                        
+                        // modified: check if the used ask to add more than one box with the same expiration date
+                        Section {
+                            ForEach(1...numerobox, id: \.self) { index in
+                                
+                                HStack {
+                                    DatePicker("Box  \(index)", selection:
+                                                  
+                                                // if the user wants all the boxes have the same expiration date,it automatically update the same date, else each box has a different expiration date
+                                               isTheSameDate ? $expirationDate[0] : $expirationDate[index-1]
+                                                
+                                                
+                                                , in: Date()..., displayedComponents: .date)
+                                    
+                                    Spacer()
+                                    
+                                    Button {} label: {
+                                        Image(systemName: "viewfinder") // add the possibility to scan
+                                    }
+                                }
+                                
+                            }
+                        }
+                        .listRowBackground(Color.init(red: 247/255, green: 213/255, blue: 223/255)).padding()
+                        // : section for stepper
+                        
+                        
+    //                    Text("Categoria")
+    //                        .font(.title2)
+    //                        .fontWeight(.semibold)
+    //                        .multilineTextAlignment(.leading)
+    //
+    //
+    //                    VStack(spacing: 20) {
+    //                        Picker(selection: $categoria, label: Text("Categoria Medicinale")) {
+    //                            Text("Antistaminici").tag(1)
+    //                            Text("Antivirali").tag(2)
+    //                            Text("Anti-Infiammatori").tag(3)
+    //                            Text("Antibiotici").tag(4)
+    //                            Text("Antimicotici").tag(5)
+    //                            Text("Analgesici").tag(6)
+    //                        }
+    //                    }
+                        
+                        Spacer()
+                        
+                        
+                        // "Done Button" it allows to add an element inside the cabinet view
+                        HStack {
+                        
+                            Spacer()
+                        Button("Conferma") {
+                            
+                            medicineViewModel.addNewMedicine(name: nome, dosage: dosaggio, type: tipologia, price: prezzo, units: Int(unità) ?? 0, category: "antistaminico", isPinned: false)
                             
                             
                             
@@ -195,57 +188,30 @@ struct NewItemView: View {
                                 boxViewModel.addNewBox(medicine: nome, expirationDate: expirationDate[i-1], state: .usable)
                             }
                             
-                            showData = false
-                        }, label: {
-                            RoundedRectangle(cornerRadius: 10)
-                                .frame(width: 300, height: 50, alignment: .center)
-                            
-                                .overlay(Text("Conferma").foregroundColor(.white))
-                        })
-//                        .disabled(nome.isEmpty || dosaggio.isEmpty || tipologia.isEmpty || tipologia.isEmpty || prezzo.isEmpty || unità.isEmpty ).padding()
+                        }
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.accentColor)
+                        .cornerRadius(8)
+                        
+                            Spacer()
+                        }
                         
                         
                         
-                        
-                        
-//                    Button("Conferma") {
-//                        
-//                        medicineViewModel.addNewMedicine(name: nome, dosage: dosaggio, type: tipologia, price: prezzo, units: Int(unità) ?? 0, category: "antistaminico")
-//                        
-//                        
-//                        
-//                        // add all the boxes of that medicine
-//                        for i in 1...numerobox {
-//                            boxViewModel.addNewBox(medicine: nome, expirationDate: expirationDate[i-1], state: .usable)
-//                        }
-//                        
-//                        showData = false
-//                        
-//                    }.disabled(self.nome.isEmpty)
-//                    .foregroundColor(.white)
-//                    .padding()
-//                    .background(Color.accentColor)
-//                    .cornerRadius(8)
+                    } // :form
+                    .onAppear(perform: {
+                        UITableView.appearance().backgroundColor = UIColor.clear
+                        UITableViewCell.appearance().backgroundColor = UIColor.clear
+                    })
                     
-//                        Spacer()
-                    
-                    
-                    
-                    
-                } // :form
-                .onAppear(perform: {
-                    UITableView.appearance().backgroundColor = UIColor.clear
-                    UITableViewCell.appearance().backgroundColor = UIColor.clear
-                })
-                .focused($amountIsFocused)
+                }
+                
+                .navigationTitle("Nuovo medicinale")
                 
             }
-            
-            .navigationTitle("Nuovo medicinale")
-            .onTapGesture {amountIsFocused = false}
         }
     }
-}
 
 
 //struct NewItemView_Previews: PreviewProvider {
